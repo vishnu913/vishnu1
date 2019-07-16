f7befa38d76e0b4e5debe10d2a4ea080f1dc6eaa
vishnu vardhan reddy <51699342+vishnu913@users.noreply.github.com>
2019-07-16 08:54:21 +0530

    Update vishnu.c

--- vishnu.c
+++ vishnu.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
- 
 void main()
 {
-    int num;
- 
-    printf("\n");
-    scanf("%d", &num);
-    if (num > 0)
-        printf("%d  positive\n", num);
-    else if (num < 0)
-        printf("%dnegative \n", num);
+    int a;
+    scanf("%d", &a);
+    if (a > 0)
+        printf("Positive");
+    else if (a < 0)
+        printf("Negative");
     else
-        printf(" zero");
+        printf("Zero");
 }
